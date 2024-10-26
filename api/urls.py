1fa16678774e8e58e3ae00f40d93124f70396ce4
Kwanghoon Son <kwangson@yahoo.com>
2024-10-26 12:36:47 +0900

    Add api point
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework import routers
+
+# v1/users
+# POST v1/users -> create user
+# v1/messages
+# POST v1/news/subscriptions -> subscribe
+# DELETE v1/news/subscriptions -> unsubscribe
+
+urlpatterns = []
