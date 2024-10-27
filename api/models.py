c28e9d7363f73f23f9ff99664542de8315a805d8
Kwanghoon Son <kwangson@yahoo.com>
2024-10-27 21:19:15 +0900

    Update model and serializer
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class UserBaseSettings(models.Model):
+    class Meta:
+        abstract = True
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin, UserBaseSettings):
+    MAX_NAME_LENGTH = 100
+    MIN_NAME_LENGTH = 2
--- api/serializers.py
+++ api/serializers.py
@@ -1,1 +1,8 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+
+
+class MarketingSubscriberSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ["email"]
