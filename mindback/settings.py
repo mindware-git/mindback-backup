3afd13e49d65dc5d19c3ac5e41a0481243c0189c
Kwanghoon Son <kwangson@yahoo.com>
2024-10-29 21:15:16 +0900

    Enable cors
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- mindback/settings.py
+++ mindback/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "api",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -49,6 +52,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://unione.site",
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = "mindback.urls"
